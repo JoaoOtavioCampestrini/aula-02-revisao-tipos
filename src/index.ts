055f54092a00d98bbd7dd573eea844ef7fc0279a
Joao Otavio <joaootaviocampestrini@gmail.com>
2024-11-13 21:32:27 -0300

    revisao-palavras

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,67 @@
-console.log("za warudo");
+// isso é um comentário
+
+/**
+ * isso também é um comentário
+ */
+
+// tipos de dados
+// number ou numérico (inteiros)
+let valor: number = 1;
+
+//boolean ou booleano (falso ou verdadeiro)
+let valorBooleano: boolean = false;
+
+// string ou cadeia de caracteres
+let valorTexto: string = "Olá Pessoal";
+
+// tipo não definido
+let valorIndefinido: undefined;
+
+// tipo nulo
+let valorNulo: null;
+
+const outroValor: 0;
+
+console.log("Mensagem no terminal");
+
+console.log("Olá mundo!");
+
+// palavras reservadas
+
+if (1) {
+} else {
+    console.log("");
+}
+
+while (0) { }
+
+do { } while (0);
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+}
+
+const chave = 10;
+
+switch (chave) {
+    case 10:
+        console.log("Chave 10");
+        break;
+        default:
+            console.log("chave inválida");
+            break;
+}
+
+function teste (valor: string) {
+    return "";
+}
+
+//let novoValor: number;
+let novoValor = 5 + 3;
+//novoValor = "5";
+novoValor = 5;
+console.log();
+
+//number
+//valor
+//double, float, numero com casas decimais
